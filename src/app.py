2d2fb9d71066e289e7f1bb90dba25b3bea7a5ca2
Ved Mahadeo <vedkailash1@gmail.com>
2023-01-31 21:55:29 -0400

    started writing code for loading data

--- src/app.py
+++ src/app.py
@@ -1,2 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
 import tensorflow as tf
-print(tf.__version__)
+
+from tensorflow import keras
+from keras import layers
+from keras.models import Sequential
+
+import pathlib
+
+import os
+
+data_dir = pathlib.Path('plantvillage')
+
+folders = os.listdir(data_dir)
+
+for folder in folders:
+    dir = pathlib.Path('plantvillage/'+folder)
+    print(   len(list(dir.glob('*.JPG')))  )
